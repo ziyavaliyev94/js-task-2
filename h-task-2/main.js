50096b95fc375515f4ef29761e09ba94d361942b
ziyavaliyev94 <ziya.valiyev.1994@gmail.com>
2024-10-28 01:39:46 +0400

    home-task-2

---
+++ h-task-2/main.js
@@ -0,0 +1,49 @@
+// sual-1
+
+// let rows = 5;
+
+// for (let i = 1; i <= rows; i++) {
+//     let stars = '*'.repeat(i);
+//     console.log(stars); 
+// }
+
+// sual-2
+
+// let rows = 5;
+
+// for (let i = rows; i >= 1; i--) {
+//     let stars = '*'.repeat(i);
+//     console.log(stars);
+// }
+
+// sual-3
+
+// let name1 = prompt("adinizi yazin")
+// let name2 = prompt("soyadinizi yazin")
+// let age1 = prompt("dogum ilinizi yazin")
+// console.log(name1  +  name2  +  "siz"  +  age1  +  'cu ilde anadan olmusunuz');
+
+// sual-4
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 5 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// sual-5
+
+// let correctUsername = "abc";
+// let correctPassword = "abc1";
+
+// let username = prompt("istifadeci adi");
+// let password = prompt("parol");
+
+// if (username == correctUsername && password == correctPassword) {
+//     console.log(`Xoş gelmisiniz`);
+// } else {
+//     console.log("İstifadeci adi ve ya parol yanlisdir");
+// }
+
+
